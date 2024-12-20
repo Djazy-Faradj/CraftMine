6846d44e7a759196eda571ec79a9e94b64a54d28
Djazy-Faradj <djazy.faradj@outlook.com>
2024-12-20 02:18:51 -0500

    First commit

---
+++ CraftMine/src/AppMain.java
@@ -0,0 +1,12 @@
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		System.out.println("Running...");
+		
+		TriangleRenderer tr = new TriangleRenderer();
+		
+		tr.Run();
+	}
+
+}
